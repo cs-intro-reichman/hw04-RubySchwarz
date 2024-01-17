3d5285403194e220deb4daf5df9d1e7736b4d910
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2024-01-17 12:50:02 +0200

    Add code hw4 revised

--- StringOps.java
+++ StringOps.java
@@ -1,58 +1,43 @@
 public class StringOps {
-    // Add methods here according to the skeleton
 
-    // Function to capitalize vowels and lowercase other characters
-    public static String capVowelsLowRest(String str) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if ("AEIOUaeiou".indexOf(ch) >= 0) {
-                result.append(Character.toUpperCase(ch));
-            } else {
-                result.append(Character.toLowerCase(ch));
-            }
-        }
-        return result.toString();
-    }
+    // ... [Other methods] ...
 
-    // Function to convert string into camelCase
+    // Revised Function to convert string into camelCase
     public static String camelCase(String str) {
         StringBuilder result = new StringBuilder();
-        boolean newWord = true;
+        boolean newWord = false;
+        boolean isFirstWord = true;
+
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (ch == ' ') {
                 newWord = true;
             } else if (newWord) {
-                result.append(i == 0 ? Character.toLowerCase(ch) : Character.toUpperCase(ch));
+                if (isFirstWord) {
+                    result.append(Character.toLowerCase(ch));
+                    isFirstWord = false;
+                } else {
+                    result.append(Character.toUpperCase(ch));
+                }
                 newWord = false;
             } else {
-                result.append(Character.toLowerCase(ch));
+                result.append(isFirstWord ? Character.toLowerCase(ch) : ch);
+                isFirstWord = false;
             }
         }
         return result.toString();
     }
 
-    // Function to find all indices of a character in a string
-    public static int[] allIndexOf(String str, char ch) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) {
-                count++;
-            }
-        }
-
-        int[] indices = new int[count];
-        int index = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) {
-                indices[index++] = i;
-            }
-        }
-        return indices;
-    }
+    // ... [Other methods] ...
 
     public static void main(String[] args) {
-        // Test the methods with example data
+        // Testing the camelCase method
+        System.out.println(camelCase(" tWo    wordS")); // "twoWords"
+
+        // Testing the allIndexOf method
+        int[] indices = allIndexOf("Hello world", 'l');
+        for (int index : indices) {
+            System.out.print(index + " ");
+        }
     }
 }
