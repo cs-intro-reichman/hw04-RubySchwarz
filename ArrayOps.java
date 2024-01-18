86bfd443b996525e32793b624b9a54928a8a4271
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2024-01-18 16:25:18 +0200

    Add code hw4 revised 14

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,6 +1,17 @@
 public class ArrayOps {
 
-    // Corrected Method to find the second maximum value in an array
+    // Method to find the missing integer in an array
+    public static int findMissingInt(int[] array) {
+        int n = array.length;
+        int totalSum = n * (n + 1) / 2;
+        int arraySum = 0;
+        for (int num : array) {
+            arraySum += num;
+        }
+        return totalSum - arraySum;
+    }
+
+    // Method to find the second maximum value in an array
     public static int secondMaxValue(int[] array) {
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
@@ -12,23 +23,41 @@ public class ArrayOps {
                 secondMax = num;
             }
         }
-        return (secondMax == Integer.MIN_VALUE) ? max : secondMax;
+        return secondMax;
     }
 
-    // Corrected Method to check if two arrays contain the same elements
+    // Method to check if two arrays contain the same elements
     public static boolean containsTheSameElements(int[] array1, int[] array2) {
-        java.util.Map<Integer, Integer> countMap1 = new java.util.HashMap<>();
-        java.util.Map<Integer, Integer> countMap2 = new java.util.HashMap<>();
+        java.util.Map<Integer, Integer> countMap = new java.util.HashMap<>();
         for (int num : array1) {
-            countMap1.put(num, countMap1.getOrDefault(num, 0) + 1);
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
         for (int num : array2) {
-            countMap2.put(num, countMap2.getOrDefault(num, 0) + 1);
+            if (!countMap.containsKey(num)) {
+                return false;
+            }
+            countMap.put(num, countMap.get(num) - 1);
+            if (countMap.get(num) == 0) {
+                countMap.remove(num);
+            }
         }
-        return countMap1.equals(countMap2);
+        return countMap.isEmpty();
     }
 
-    // Other methods (findMissingInt, isSorted) remain unchanged
+    // Method to check if an array is sorted (either increasing or decreasing order)
+    public static boolean isSorted(int[] array) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                increasing = false;
+            }
+            if (array[i] < array[i + 1]) {
+                decreasing = false;
+            }
+        }
+        return increasing || decreasing;
+    }
 
     // Main method
     public static void main(String[] args) {
