c81e4beeca9067226a7a7c56e529bcf2d9b5a7fc
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2024-01-18 16:14:51 +0200

    Add code hw4 revised 11

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,22 +1,20 @@
 public class ArrayOps {
-    public static void main(String[] args) {
-        // Test the methods with example data
-    }
 
+    // Method to find the missing integer in an array
     public static int findMissingInt(int[] array) {
         int n = array.length;
         int totalSum = n * (n + 1) / 2;
-        int sum = 0;
+        int arraySum = 0;
         for (int num : array) {
-            sum += num;
+            arraySum += num;
         }
-        return totalSum - sum;
+        return totalSum - arraySum;
     }
 
+    // Method to find the second maximum value in an array
     public static int secondMaxValue(int[] array) {
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
-
         for (int num : array) {
             if (num > max) {
                 secondMax = max;
@@ -25,28 +23,28 @@ public class ArrayOps {
                 secondMax = num;
             }
         }
-        
-        if (secondMax == Integer.MIN_VALUE) {
-            return max; // In case all elements are the same
-        }
-
         return secondMax;
     }
 
+    // Method to check if two arrays contain the same elements
     public static boolean containsTheSameElements(int[] array1, int[] array2) {
-        java.util.HashMap<Integer, Integer> counts1 = new java.util.HashMap<>();
+        java.util.Map<Integer, Integer> countMap = new java.util.HashMap<>();
         for (int num : array1) {
-            counts1.put(num, counts1.getOrDefault(num, 0) + 1);
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-
-        java.util.HashMap<Integer, Integer> counts2 = new java.util.HashMap<>();
         for (int num : array2) {
-            counts2.put(num, counts2.getOrDefault(num, 0) + 1);
+            if (!countMap.containsKey(num)) {
+                return false;
+            }
+            countMap.put(num, countMap.get(num) - 1);
+            if (countMap.get(num) == 0) {
+                countMap.remove(num);
+            }
         }
-
-        return counts1.equals(counts2);
+        return countMap.isEmpty();
     }
 
+    // Method to check if an array is sorted (either increasing or decreasing order)
     public static boolean isSorted(int[] array) {
         boolean increasing = true;
         boolean decreasing = true;
@@ -59,5 +57,10 @@ public class ArrayOps {
             }
         }
         return increasing || decreasing;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // The main method can be used for testing the methods
     }
 }
--- StringOps.java
+++ StringOps.java
@@ -1,39 +1,39 @@
 public class StringOps {
-    // Add methods here according to the skeleton
-
-    // Function to capitalize vowels and lowercase other characters
+    // Method to capitalize vowels and lower case the rest
     public static String capVowelsLowRest(String str) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if ("AEIOUaeiou".indexOf(ch) >= 0) {
-                result.append(Character.toUpperCase(ch));
+            char c = str.charAt(i);
+            if ("AEIOUaeiou".indexOf(c) >= 0) {
+                result.append(Character.toUpperCase(c));
             } else {
-                result.append(Character.toLowerCase(ch));
+                result.append(Character.toLowerCase(c));
             }
         }
         return result.toString();
     }
 
-    // Function to convert string into camelCase
+    // Method to convert to camel case
     public static String camelCase(String str) {
         StringBuilder result = new StringBuilder();
-        boolean newWord = true;
+        boolean nextUpper = false;
         for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == ' ') {
-                newWord = true;
-            } else if (newWord) {
-                result.append(i == 0 ? Character.toLowerCase(ch) : Character.toUpperCase(ch));
-                newWord = false;
+            char c = str.charAt(i);
+            if (c == ' ') {
+                nextUpper = true;
+                continue;
+            }
+            if (result.length() == 0 || nextUpper) {
+                result.append(Character.toUpperCase(c));
+                nextUpper = false;
             } else {
-                result.append(Character.toLowerCase(ch));
+                result.append(Character.toLowerCase(c));
             }
         }
         return result.toString();
     }
 
-    // Function to find all indices of a character in a string
+    // Method to find all indexes of a given character
     public static int[] allIndexOf(String str, char ch) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -42,17 +42,15 @@ public class StringOps {
             }
         }
 
-        int[] indices = new int[count];
-        int index = 0;
+        int[] indexes = new int[count];
+        int idx = 0;
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ch) {
-                indices[index++] = i;
+                indexes[idx++] = i;
             }
         }
-        return indices;
+
+        return indexes;
     }
 
-    public static void main(String[] args) {
-        // Test the methods with example data
-    }
 }
