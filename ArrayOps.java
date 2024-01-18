bb0e1717b9a88beefff0ccd93f6df73b0d7c8121
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2024-01-18 16:55:32 +0200

    Add code hw4 revised 9

--- ArrayOps.java
+++ ArrayOps.java
@@ -15,16 +15,16 @@ public class ArrayOps {
     public static int secondMaxValue(int[] arr) {
          int firstMax = Integer.MIN_VALUE;
          int secondMax = Integer.MIN_VALUE;
-    
+
          for (int num : arr) {
-            if (num > firstMax) {
-                 secondMax = firstMax;
-                 firstMax = num;
-            } else if (num > secondMax && num < firstMax) {
-                secondMax = num;
+           if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num != firstMax) {
+            secondMax = num;
         }
     }
-    
+
     return secondMax;
 }
 
